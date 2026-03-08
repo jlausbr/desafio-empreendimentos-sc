aba20faa6cc6e3dfcdc64c5a9991ead1915a53a3
Jordan Laus <lausjordan@gmail.com>
2026-03-08 19:08:40 -0300

    test: adiciona smoke test para validação do contexto Spring Boot

---
+++ src/test/java/br/com/sctec/desafiopratico/EmpreendimentosScApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.sctec.desafiopratico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpreendimentosScApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
