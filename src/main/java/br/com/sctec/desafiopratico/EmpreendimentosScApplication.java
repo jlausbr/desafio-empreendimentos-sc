696151f7ffb5bdde9197c2073c3331a0d1ca9208
Jordan Laus <lausjordan@gmail.com>
2026-03-08 14:22:47 -0300

    chore: inicializa projeto Spring Boot com dependências

---
+++ src/main/java/br/com/sctec/desafiopratico/EmpreendimentosScApplication.java
@@ -0,0 +1,12 @@
+package br.com.sctec.desafiopratico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpreendimentosScApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpreendimentosScApplication.class, args);
+    }
+}
